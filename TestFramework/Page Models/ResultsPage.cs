556fb24397dfe188898a559fc3ad664438b1bedd
unknown <mrb@StarSong.ecocion.net>
2014-04-25 13:57:57 -0600

    Full proj commit

---
+++ TestFramework/Page Models/ResultsPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+
+namespace TestFramework {
+    public class ResultsPage
+    {
+
+        [FindsBy(How = How.CssSelector, Using = ".srg .g .r a")]
+        public IList<IWebElement> elements;
+
+        
+    }  
+}
