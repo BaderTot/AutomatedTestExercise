f39db37bb96ea594df64cb0b91c93e2ce45f3c1a
unknown <mrb@StarSong.ecocion.net>
2014-04-25 15:00:39 -0600

    Clean up

---
+++ Pages.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace TestFramework {
+    public class Pages {
+         
+        public static GoogleHomePage GoogleHomePage {
+            get {               
+                var googleHomePage = new GoogleHomePage();
+                PageFactory.InitElements(Browser.Driver, googleHomePage);
+                return googleHomePage;
+            }
+        }
+
+        public static ResultsPage ResultsPage{
+            get {
+                var resultsPage = new ResultsPage();
+                PageFactory.InitElements(Browser.Driver, resultsPage);
+                return resultsPage;
+            }
+        }
+    }
+}
